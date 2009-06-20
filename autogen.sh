b06b8462fa8324cd5bb0ed3b40c7dd4297f38bc2
Ryan Lortie <desrt@desrt.ca>
2009-06-20 16:27:12 -0400

    Fix ./autogen.sh clean

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ set -e
 if [ "$1" = "clean" ]; then
   rm -f aclocal.m4 configure missing install-sh \
         depcomp ltmain.sh config.guess config.sub \
-        `find . -name Makefile.in` compile
+        `find . -name Makefile.in` compile ltmain.sh
   rm -rf autom4te.cache      
   exit
 fi
