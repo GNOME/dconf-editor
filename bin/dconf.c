5b399fdc44dc3b740577171c71daa9aac0d1c924
Ryan Lortie <desrt@desrt.ca>
2009-10-07 16:39:17 -0400

    relicense to LGPL 2.1

--- bin/dconf.c
+++ bin/dconf.c
@@ -1,8 +1,23 @@
-#include <stdio.h>
-#include <dconf.h>
-#include <getopt.h>
-#include <glib.h>
+/*
+ * Copyright © 2007, 2008  Ryan Lortie
+ * Copyright © 2009 Codethink Limited
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the licence, or (at your option) any later version.
+ *
+ * See the included COPYING file for more information.
+ *
+ * Authors: Ryan Lortie <desrt@desrt.ca>
+ */
+
 #include <string.h>
+#include <getopt.h>
+#include <stdio.h>
+
+#include <dconf.h>
+#include <glib.h>
 
 static const char usage_message[] =
 "usage: dconf [--version] [--async] [--help] COMMAND [ARGS]\n";
--- dconf/dconf.c
+++ dconf/dconf.c
@@ -1,10 +1,11 @@
 /*
  * Copyright © 2007, 2008  Ryan Lortie
- * Copyright © 2009  Codethink Limited
+ * Copyright © 2009 Codethink Limited
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of version 3 of the GNU General Public License as
- * published by the Free Software Foundation.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
+ * of the licence, or (at your option) any later version.
  *
  * See the included COPYING file for more information.
  *
