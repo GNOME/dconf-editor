GNOME/dconf-editor

commit 36785e633d45d6f086366dad90686d43eb09fe04

Remove is_key_default().

editor/dconf-model.vala
@@ -503,7 +503,7 @@ public class SettingsModel : Object
         gkey_value_push (key.full_name,
                          key.context,
                          _get_gsettings_key_value (key),
-                         _is_key_default (key));
+                         is_key_default (key));
     }
 
     private inline void push_dconf_key_value (string full_name, DConf.Client client)
@@ -673,15 +673,9 @@ public class SettingsModel : Object
         }
     }
 
-    public bool is_key_default (string full_name, string schema_id)
+    private static inline bool is_key_default (GSettingsKey key)
     {
-        GSettingsKey key = get_specific_gsettings_key (full_name, schema_id);
-        return _is_key_default (key);
-    }
-    private bool _is_key_default (GSettingsKey key)
-    {
-        GLib.Settings settings = key.settings;
-        return settings.get_user_value (key.name) == null;
+        return key.settings.get_user_value (key.name) == null;
     }
 
     public bool key_has_schema (string full_name)

editor/registry-info.vala
@@ -120,43 +120,43 @@ class RegistryInfo : Grid, BrowsableView
 
         // TODO g_variant_dict_lookup_value() return value is not annotated as nullable
         string type_code;
-        if (!properties.lookup ("type-code",    "s",    out type_code))
+        if (!properties.lookup ("type-code",        "s",    out type_code))
             assert_not_reached ();
 
         bool test;
         string tmp_string;
 
-        if (properties.lookup ("defined-by",    "s",    out tmp_string))
-            add_row_from_label (_("Defined by"),            tmp_string);
+        if (properties.lookup ("defined-by",        "s",    out tmp_string))
+            add_row_from_label (_("Defined by"),                tmp_string);
         else assert_not_reached ();
 
-        if (properties.lookup ("schema-id",     "s",    out tmp_string))
-            add_row_from_label (_("Schema"),                tmp_string);
+        if (properties.lookup ("schema-id",         "s",    out tmp_string))
+            add_row_from_label (_("Schema"),                    tmp_string);
 
         add_separator ();
-        if (properties.lookup ("summary",       "s",    out tmp_string))
+        if (properties.lookup ("summary",           "s",    out tmp_string))
         {
             test = tmp_string == "";
             add_row_from_label (_("Summary"),
-                                test ?                      _("No summary provided")
-                                     :                      tmp_string,
+                                test ?                          _("No summary provided")
+                                     :                          tmp_string,
                                 test);
         }
-        if (properties.lookup ("description",   "s",    out tmp_string))
+        if (properties.lookup ("description",       "s",    out tmp_string))
         {
             test = tmp_string == "";
             add_row_from_label (_("Description"),
-                                test ?                      _("No description provided")
-                                     :                      tmp_string,
+                                test ?                          _("No description provided")
+                                     :                          tmp_string,
                                 test);
         }
         /* Translators: as in datatype (integer, boolean, string, etc.) */
-        if (properties.lookup ("type-name",     "s",    out tmp_string))
-            add_row_from_label (_("Type"),                  tmp_string);
+        if (properties.lookup ("type-name",         "s",    out tmp_string))
+            add_row_from_label (_("Type"),                      tmp_string);
         else assert_not_reached ();
 
         bool range_type_is_range = false;
-        if (properties.lookup ("range-type",    "s",    out tmp_string)) // has_schema
+        if (properties.lookup ("range-type",        "s",    out tmp_string)) // has_schema
         {
             if (type_code == "d" || type_code == "y"    // double and unsigned 8 bits; not the handle type
              || type_code == "i" || type_code == "u"    // signed and unsigned 32 bits
@@ -164,22 +164,28 @@ class RegistryInfo : Grid, BrowsableView
              || type_code == "x" || type_code == "t")   // signed and unsigned 64 bits
             {
                 range_type_is_range = tmp_string == "range";
-                add_row_from_label (_("Forced range"),      Key.cool_boolean_text_value (range_type_is_range));
+                add_row_from_label (_("Forced range"),          Key.cool_boolean_text_value (range_type_is_range));
             }
         }
 
         bool minimum_is_maximum = false;
         string tmp = "";
         tmp_string = "";
-        if (properties.lookup ("minimum",       "s",    out tmp_string))
-            add_row_from_label (_("Minimum"),               tmp_string);
-        if (properties.lookup ("maximum",       "s",    out tmp       ))
-            add_row_from_label (_("Maximum"),               tmp       );
+        if (properties.lookup ("minimum",           "s",    out tmp_string))
+            add_row_from_label (_("Minimum"),                   tmp_string);
+        if (properties.lookup ("maximum",           "s",    out tmp       ))
+            add_row_from_label (_("Maximum"),                   tmp       );
         if (tmp != "" && tmp == tmp_string)
             minimum_is_maximum = true;
 
-        if (properties.lookup ("default-value", "s",    out tmp_string))
-            add_row_from_label (_("Default"),               tmp_string);
+        bool has_schema_and_is_default;
+        if (!has_schema)
+            has_schema_and_is_default = false;
+        else if (!properties.lookup ("is-default",  "b",    out has_schema_and_is_default))
+            assert_not_reached ();
+
+        if (properties.lookup ("default-value",     "s",    out tmp_string))
+            add_row_from_label (_("Default"),                   tmp_string);
 
         if (has_schema && error_hard_conflicting_key)
         {
@@ -188,7 +194,7 @@ class RegistryInfo : Grid, BrowsableView
         }
         else
         {
-            if (has_schema && model.is_key_default (full_name, context))
+            if (has_schema_and_is_default)
                 current_value_label = new Label (get_current_value_text (null));
             else if (has_schema)
                 current_value_label = new Label (get_current_value_text (model.get_gsettings_key_value (full_name, context)));
@@ -290,7 +296,7 @@ class RegistryInfo : Grid, BrowsableView
 
             bool planned_change = modifications_handler.key_has_planned_change (full_name);
             Variant? planned_value = modifications_handler.get_key_planned_value (full_name);
-            bool key_value_is_default = planned_change ? planned_value == null : model.is_key_default (full_name, context);
+            bool key_value_is_default = planned_change ? planned_value == null : has_schema_and_is_default;
             custom_value_switch.set_active (key_value_is_default);
 
             ulong switch_active_handler = custom_value_switch.notify ["active"].connect (() => {
@@ -322,7 +328,12 @@ class RegistryInfo : Grid, BrowsableView
                 });
             revealer_reload_1_handler = modifications_handler.leave_delay_mode.connect (() => {
                     SignalHandler.block (custom_value_switch, switch_active_handler);
-                    custom_value_switch.set_active (model.is_key_default (full_name, context));
+
+                    VariantDict local_properties = new VariantDict (model.get_key_properties (full_name, context, {"is-default"}));
+                    bool is_key_default;
+                    if (!local_properties.lookup ("is-default", "b", out is_key_default))
+                        assert_not_reached ();
+                    custom_value_switch.set_active (is_key_default);
                     SignalHandler.unblock (custom_value_switch, switch_active_handler);
                 });
             custom_value_switch.destroy.connect (() => custom_value_switch.disconnect (switch_active_handler));
@@ -493,13 +504,12 @@ class RegistryInfo : Grid, BrowsableView
     * * Updating value
     \*/
 
-    public void gkey_value_push (Variant key_value, bool is_key_default)
+    public void gkey_value_push (Variant key_value, bool is_key_default)    // TODO check if there isn't a problem on conflicting keys
     {
-        SettingsModel model = modifications_handler.model;
-        if (model.is_key_default (full_name, context))
+        if (is_key_default)
             current_value_label.set_text (get_current_value_text (null));
         else
-            current_value_label.set_text (get_current_value_text (model.get_gsettings_key_value (full_name, context)));
+            current_value_label.set_text (get_current_value_text (key_value));
     }
 
     public void dkey_value_push (Variant? key_value_or_null)

editor/modifications-revealer.vala
@@ -110,7 +110,11 @@ class ModificationsRevealer : Revealer
             else
             {
                 string schema_id = objects [position, 0];
-                if (!model.is_key_default (full_name, schema_id))
+                VariantDict properties = new VariantDict (model.get_key_properties (full_name, schema_id, {"is-default"}));
+                bool is_key_default;
+                if (!properties.lookup ("is-default", "b", out is_key_default))
+                    assert_not_reached ();
+                if (!is_key_default)
                     modifications_handler.add_delayed_setting (full_name, null, schema_id);
             }
         }
@@ -164,10 +168,21 @@ class ModificationsRevealer : Revealer
         string full_name = ((SimpleSettingObject) object).full_name;
         string context = ((SimpleSettingObject) object).context;
 
-        bool has_schema = context != ".dconf";
         SettingsModel model = modifications_handler.model;
 
-        bool is_default_or_ghost = has_schema ? model.is_key_default (full_name, context)
+        VariantDict properties = new VariantDict (model.get_key_properties (full_name, context, {"has-schema", "is-default", "default-value"}));
+
+        bool has_schema;
+        if (!properties.lookup ("has-schema",       "b",    out has_schema))
+            assert_not_reached ();
+
+        bool has_schema_and_is_default;
+        if (!has_schema)
+            has_schema_and_is_default = false;
+        else if (!properties.lookup ("is-default",  "b",    out has_schema_and_is_default))
+            assert_not_reached ();
+
+        bool is_default_or_ghost = has_schema ? has_schema_and_is_default
                                               : model.is_key_ghost (full_name);
         Variant? planned_value = modifications_handler.get_key_planned_value (full_name);
         string? cool_planned_value = null;
@@ -176,16 +191,11 @@ class ModificationsRevealer : Revealer
 
         string? cool_default_value = null;
         Variant key_value;
-        if (has_schema)
-        {
-            VariantDict properties = new VariantDict (model.get_key_properties (full_name, context, {"default-value"}));
-            if (!properties.lookup ("default-value", "s", out cool_default_value))
-                assert_not_reached ();
-
-            key_value = modifications_handler.model.get_gsettings_key_value (full_name, context);
-        }
+        if (has_schema
+         && properties.lookup ("default-value", "s",    out cool_default_value))
+            key_value = model.get_gsettings_key_value (full_name, context);
         else
-            key_value = modifications_handler.model.get_dconf_key_value (full_name);
+            key_value = model.get_dconf_key_value (full_name);
 
         DelayedSettingView view = new DelayedSettingView (full_name,
                                                           context,
@@ -251,7 +261,7 @@ class ModificationsRevealer : Revealer
     }
 
     /*\
-    * * Updating values
+    * * Updating values; TODO only works for watched keys...
     \*/
 
     public void gkey_value_push (string full_name, string schema_id, Variant key_value, bool is_key_default)

editor/registry-list.vala
@@ -603,15 +603,17 @@ private abstract class RegistryList : Grid, BrowsableView
 
         VariantDict properties = new VariantDict (model.get_key_properties (full_name,
                                                                             schema_id,
-                                                                            {"type-code", "range-type", "range-content", "hard-conflict"}));
+                                                                            {"type-code", "range-type", "range-content", "is-default", "hard-conflict"}));
         string type_string, range_type;
         Variant range_content;
-        bool error_hard_conflicting_key;
+        bool is_key_default, error_hard_conflicting_key;
         if (!properties.lookup ("type-code",     "s", out type_string))
             assert_not_reached ();
         if (!properties.lookup ("range-type",    "s", out range_type))
             assert_not_reached ();
         if (!properties.lookup ("range-content", "v", out range_content))
+            assert_not_reached ();
+        if (!properties.lookup ("is-default",    "b", out is_key_default))
             assert_not_reached ();
         if (!properties.lookup ("hard-conflict", "b", out error_hard_conflicting_key))
             assert_not_reached ();
@@ -647,7 +649,7 @@ private abstract class RegistryList : Grid, BrowsableView
             if (planned_change)
                 action = popover.create_buttons_list (true, delayed_apply_menu, planned_change, type_string, range_content,
                                                       modifications_handler.get_key_planned_value (full_name));
-            else if (model.is_key_default (full_name, schema_id))
+            else if (is_key_default)
                 action = popover.create_buttons_list (true, delayed_apply_menu, planned_change, type_string, range_content,
                                                       null);
             else
@@ -670,7 +672,7 @@ private abstract class RegistryList : Grid, BrowsableView
         {
             popover.new_section ();
 
-            if (!model.is_key_default (full_name, schema_id))
+            if (!is_key_default)
                 popover.new_gaction ("default2", "bro.set-to-default(" + variant_ss.print (false) + ")");
 
             string [] all_flags = range_content.get_strv ();
@@ -692,7 +694,7 @@ private abstract class RegistryList : Grid, BrowsableView
             if (planned_value != null)
                 popover.new_gaction ("default1", "bro.set-to-default(" + variant_ss.print (false) + ")");
         }
-        else if (!model.is_key_default (full_name, schema_id))
+        else if (!is_key_default)
         {
             popover.new_section ();
             popover.new_gaction ("default1", "bro.set-to-default(" + variant_ss.print (false) + ")");

editor/setting-object.vala
@@ -448,6 +448,8 @@ public class GSettingsKey : Key
             variantdict.insert_value ("soft-conflict",              new Variant.boolean (warning_conflicting_key));
         if (all_properties_queried || "hard-conflict"   in query)
             variantdict.insert_value ("hard-conflict",              new Variant.boolean (error_hard_conflicting_key));
+        if (all_properties_queried || "is-default"      in query)
+            variantdict.insert_value ("is-default",                 new Variant.boolean (settings.get_user_value (name) == null));
 
         return variantdict.end ();
     }

commit 733d7e6e69f0c3fe9f7d6330e0749bc7a37bf476

Do not suggest to reload view after key is edited.
Regressions from 36785e633d and 98f70fd96d.

editor/browser-view.vala
@@ -239,9 +239,14 @@ class BrowserView : Grid
         }
         else if (type == ViewType.OBJECT)
         {
-            if (model.key_exists (path, last_context)
-             && !current_child.check_reload_object (model.get_key_properties (path, last_context, {})))
-                return false;
+            if (model.key_exists (path, last_context))
+            {
+                VariantDict properties = new VariantDict (model.get_key_properties (path, last_context, {}));
+                properties.remove ("is-default");
+                properties.remove ("key-value");
+                if (!current_child.check_reload_object (properties.end ()))
+                    return false;
+            }
             if (show_infobar)
             {
                 info_bar.show_warning ("hard-reload-object");

editor/dconf-model.vala
@@ -466,13 +466,9 @@ public class SettingsModel : Object
     private void add_watched_key (Key key)
     {
         if (key is GSettingsKey)
-            key.key_value_changed_handler = key.value_changed.connect (() => {
-                    push_gsettings_key_value ((GSettingsKey) key);
-                });
+            key.key_value_changed_handler = key.value_changed.connect (() => push_gsettings_key_value ((GSettingsKey) key));
         else if (key is DConfKey)
-            key.key_value_changed_handler = key.value_changed.connect (() => {
-                    push_dconf_key_value (key.full_name, client);
-                });
+            key.key_value_changed_handler = key.value_changed.connect (() => push_dconf_key_value (key.full_name, client));
         else assert_not_reached ();
 
         watched_keys.append (key);

editor/registry-info.vala
@@ -70,13 +70,18 @@ class RegistryInfo : Grid, BrowsableView
     {
         full_name = _full_name;
         context = _context;
-        current_key_info = _current_key_info;
+
+        VariantDict properties = new VariantDict (_current_key_info);
+        properties.remove ("is-default");
+        properties.remove ("key-value");
+        current_key_info = properties.end ();
+        properties.clear ();
 
         SettingsModel model = modifications_handler.model;
 
         clean ();   // for when switching between two keys, for example with a search (maybe also bookmarks)
 
-        VariantDict properties = new VariantDict (_current_key_info);
+        properties = new VariantDict (_current_key_info);
 
         bool has_schema;
         if (!properties.lookup ("has-schema",       "b",    out has_schema))